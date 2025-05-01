0f6c248e32bd2116be4f9db4b5c9e46f364e3866
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2025-05-01 21:05:12 +0700

    shadcn/ui

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -24,6 +25,9 @@ export default function Home() {
             Chào NextJS
           </li>
         </ol>
+        <div>
+          <Button>Click me</Button>
+        </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
